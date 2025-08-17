5a68950ef359f2a911c2fbbb7e8dcbec31248e28
Diego López Ariza <226767005+veronicafloresutim-spec@users.noreply.github.com>
2025-08-17 09:06:44 -0600

    Primer commit
    Proyecto hecho con Maven y JavaFX

---
+++ src/main/java/org/example/cobra_te/Launcher.java
@@ -0,0 +1,9 @@
+package org.example.cobra_te;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
