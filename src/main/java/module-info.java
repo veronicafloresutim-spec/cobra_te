889e840683ce5171f1535c514dbe4d30d1f77202
Diego López Ariza <diegoawesome2017@outlook.com>
2025-08-20 00:09:46 -0600

    Fixed pom.xml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,30 +1,14 @@
 module org.example.cobra_te {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
-
-    // Requerimientos para la base de datos
     requires java.sql;
 
     opens org.example.cobra_te to javafx.fxml;
     opens org.example.cobra_te.controllers to javafx.fxml;
     opens org.example.cobra_te.models to javafx.base;
+    opens org.example.cobra_te.utils to javafx.fxml;
 
     exports org.example.cobra_te;
-
-    // Exportar paquetes de base de datos
-    exports org.example.cobra_te.database;
-    exports org.example.cobra_te.models;
-    exports org.example.cobra_te.dao;
-    exports org.example.cobra_te.examples;
     exports org.example.cobra_te.controllers;
     exports org.example.cobra_te.utils;
 }
